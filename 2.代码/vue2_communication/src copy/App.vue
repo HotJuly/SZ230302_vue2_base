85d8e7b033092b226a9263166edbbf1c8c0717eb
HotJuly <949588380@qq.com>
2023-07-17 21:54:07 +0800

    first commit

---
+++ 2.代码/vue2_communication/src copy/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>我是APP组件</h1>
+
+    <Child1></Child1>
+    
+    <Child2></Child2>
+  </div>
+</template>
+
+<script>
+import Child1 from './components/Child1.vue'
+import Child2 from './components/Child2.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Child1,
+    Child2
+  }
+}
+</script>
+
+<style>
+#app{
+  width:80%;
+  padding:20px;
+  border:1px solid;
+}
+</style>
---
+++ 2.代码/vue2_communication/src/components/GrandChild1-1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="gc">
+    <h3>我是GrandChild 1-1组件</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GrandChild1-1'
+}
+</script>
+
+<style scoped>
+.gc {
+  width:80%;
+  padding: 20px;
+  border: 1px solid;
+}
+</style>
