c4a5ddac4e2ddc730166e052d4edbd201851ae42
HotJuly <949588380@qq.com>
2023-07-18 09:20:20 +0800

    02.Vue2代码中的细节问题

--- 2.代码/vue2_communication/src/App.vue
+++ 2.代码/vue2_communication/src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h1>我是APP组件</h1>
 
+    <!-- 所以使用两次Child1,Child1是构造函数,所以会生成两个组件实例对象 -->
+    <Child1></Child1>
     <Child1></Child1>
     
     <Child2></Child2>
@@ -12,8 +14,16 @@
 import Child1 from './components/Child1.vue'
 import Child2 from './components/Child2.vue'
 
+console.log(Child1)
+
+// 每个.vue文件,暴露的其实都是组件的配置对象
+// 想要找到这个对象,可以通过this.$options快速找到
 export default {
   name: 'App',
+  mounted(){
+    // console.log('vm',this)
+  },
+  // 这里其实就是根据配置对象,来生成了对应的构造函数
   components: {
     Child1,
     Child2
